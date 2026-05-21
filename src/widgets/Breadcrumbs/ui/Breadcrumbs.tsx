8b3c681ee88c0564d14a0cf6d80589fc541c6a1b
Aleksandr Kalinin <alexander.kalinin@innowise.com>
2026-05-21 15:10:13 +0200

    fix: fix breadcrumbs typing

--- src/widgets/Breadcrumbs/ui/Breadcrumbs.tsx
+++ src/widgets/Breadcrumbs/ui/Breadcrumbs.tsx
@@ -1,17 +1,26 @@
 import { Breadcrumbs as MuiBreadcrumbs, Link, Typography } from '@mui/material'
-import { useMatches, Link as RouterLink } from 'react-router'
+import { useMatches, Link as RouterLink, type UIMatch } from 'react-router'
+
+type BreadcrumbHandle<T = unknown> = {
+  breadcrumb?: string | ((match: UIMatch<T>) => React.ReactNode)
+}
+
+type BreadcrumbMatch<T = unknown> = UIMatch<T, BreadcrumbHandle<T>>
 
 export const Breadcrumbs = () => {
   const matches = useMatches()
 
-  const breadcrumbs = matches.filter((match) => match.handle?.breadcrumb)
+  const breadcrumbs = matches.filter(
+    (match) =>
+      match.handle && 'breadcrumb' in (match.handle as BreadcrumbHandle),
+  ) as BreadcrumbMatch[]
 
   return (
     <MuiBreadcrumbs aria-label="breadcrumb">
       {breadcrumbs.map((match, index) => {
         const isLast = index === breadcrumbs.length - 1
 
-        const breadcrumb = match.handle?.breadcrumb
+        const breadcrumb = (match.handle as BreadcrumbHandle)?.breadcrumb
 
         const label =
           typeof breadcrumb === 'function' ? breadcrumb(match) : breadcrumb
